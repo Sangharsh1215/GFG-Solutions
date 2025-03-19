737c8a4ff6a354a38ef62c0ecc19cb0b551d007a
Sangharsh <135854647+Sangharsh1215@users.noreply.github.com>
2025-03-19 15:47:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProfit(self, prices, k):
+        
+        n = len(prices)
+        if n == 0:
+            return 0
+        
+        dp = [[0] * n for _ in range(k+1)]
+        
+        for i in range(1, k+1):
+            max_diff = -prices[0]
+            for j in range(1, n):
+                dp[i][j] = max(dp[i][j-1], prices[j] + max_diff)
+                max_diff = max(max_diff, dp[i-1][j] - prices[j])
+        
+        return dp[k][n-1]
+
+            
+        
+
+#{ 
+ # Driver Code Starts
+from collections import deque
+
+if __name__ == "__main__":
+    tc = int(input())
+    for _ in range(tc):
+        arr = list(map(int, input().split()))
+        k = int(input())
+        obj = Solution()
+        print(obj.maxProfit(arr, k))
+        print("~")
+# } Driver Code Ends
