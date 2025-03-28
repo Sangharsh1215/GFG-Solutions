36e74413a7192e4b872d54d473208289fc2bee78
Sangharsh <135854647+Sangharsh1215@users.noreply.github.com>
2025-03-29 03:14:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.py
@@ -0,0 +1,36 @@
+class Solution:
+    def pairWithMaxSum(self, arr):
+        max_sum = float('-inf')
+        for i in range(len(arr) - 1):
+            max_sum = max(max_sum, arr[i] + arr[i + 1])
+        return max_sum
+
+    
+
+
+        
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+        a = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.pairWithMaxSum(a))
+        print("~")
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
